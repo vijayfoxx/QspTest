d82b5c86e0142299df06f8c92ecca2449bfb41c2
VICKY <VICKY@VIGNESHRAJIV>
2022-09-16 08:28:54 +0530

    First commit by eng1

---
+++ src/test/java/practice/AddToCartTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class AddToCartTest {
+	@Test
+	public void addToCart() {
+		System.out.println("The product added successfully");
+	}
+
+}
